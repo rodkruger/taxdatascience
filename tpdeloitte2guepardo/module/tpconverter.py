06538f42c430a9f129cc2319c56c170f0e47f97e
Rodrigo Krüger <rodrigo.kruger.eng@gmail.com>
2020-06-22 13:29:52 -0300

    Generate the XLSX files directly

--- tpdeloitte2guepardo/module/tpconverter.py
+++ tpdeloitte2guepardo/module/tpconverter.py
@@ -19,13 +19,15 @@ import mimetypes
 import os.path
 
 import pandas as pd
-import xlsxwriter
 
 #######################################################################################################################
 # --- CONSTANTS
 #######################################################################################################################
 
+DATA_EXCEL_EXTENSION = '.xlsx'
 SEPARATOR = '|'
+TEMPLATE_EXCEL_EXTENSION = '.xlsx'
+TEMPLATE_EXCEL_SHEET_NAME = 'Planilha1'
 
 
 #######################################################################################################################
@@ -38,14 +40,25 @@ SEPARATOR = '|'
 #######################################################################################################################
 
 def check_source_file_type(origin_file):
-    """ Check if the source file is a text/file """
+    """
+    Check if the source file is a text/file
+
+    :param origin_file: origin file, where contains the data
+    :return: True - if the file is text/plain; False - if the file is not text/plain
+    """
 
     return mimetypes.guess_type(origin_file)[0].find('text') >= 0
 
 
-def convert_file(origin_file, target_folder):
-    """ Starts the file conversion. Checks the existence of the file and
-    target folder, and then starts the conversion """
+def convert_file(origin_file, target_folder, template_path):
+    """
+    Starts the file conversion. Checks the existence of the file and target folder, and then starts the conversion
+
+    :param origin_file: origin file, where contains the data
+    :param target_folder: the folder where the target files will be written
+    :param template_path: the path for the excel templates
+    :return: None
+    """
 
     is_validated = True
 
@@ -69,13 +82,26 @@ def convert_file(origin_file, target_folder):
         return
 
     # Everything ok! Let's do the conversion
-    blocks = make_conversion(origin_file, target_folder)
+    blocks = make_conversion(origin_file)
+
+    # Export the data to .csv file
     export_data_to_file(blocks, target_folder)
-    export_data_to_excel(blocks, target_folder)
+
+    # Read the headers from the templates
+    template_headers = read_excel_header_templates(blocks, template_path)
+
+    # Export the data to Excel File
+    export_data_to_excel(blocks, target_folder, template_headers)
 
 
 def export_data_to_file(blocks, target_folder):
-    """ Export each block in the dictionary for a specific file  """
+    """
+    Export each block in the dictionary for a specific file
+
+    :param blocks: a dictionary, where the key is the ECF block key
+    :param target_folder: the folder where the target files will be written
+    :return:
+    """
 
     # Iterate over all the block keys, and export the list with all lines for
     # a specific file
@@ -95,20 +121,43 @@ def export_data_to_file(blocks, target_folder):
             file.write(SEPARATOR.join(line_data))
 
 
-def export_data_to_excel(blocks, target_folder):
-    """ Export each block in the dictionary for a specific Excel Spreadsheet  """
+def export_data_to_excel(blocks, target_folder, template_headers):
+    """
+    Export each block in the dictionary for a specific Excel Spreadsheet
+
+    :param blocks: a dictionary, where the key is the ECF block key
+    :param target_folder: the folder where the target files will be written
+    :return:
+    """
 
     # Iterate over all the block keys, and export the list with all lines for
     # a specific file
     for block_key in blocks:
-        # Generate the target file name
-        file_name = target_folder + block_key + ".xls"
 
         # Read the data previously splitted
         block_data = blocks[block_key]
 
         # Create a Pandas Dataframe with the data
         df = pd.DataFrame(block_data)
+
+        # Verify it there is template header name
+        if block_key in template_headers:
+
+            # Get the template header names
+            template_header = template_headers[block_key]
+
+            # Let the template_header and colums with the same size. Pandas throws an exception if they are different
+            while len(template_header) < len(df.columns):
+                template_header.append("No_name")
+        
+            while len(template_header) > len(df.columns):
+                del template_header[-1]
+
+            # Put the new names into the DataFrame
+            df.columns = template_header
+
+        # Generate the target file name
+        file_name = target_folder + block_key + DATA_EXCEL_EXTENSION
 
         # Create a Writer to export the data to an Excel Spreadsheet
         writer = pd.ExcelWriter(file_name, engine="xlsxwriter")
@@ -120,10 +169,13 @@ def export_data_to_excel(blocks, target_folder):
         writer.save()
 
 
-def make_conversion(origin_file, target_folder):
+def make_conversion(origin_file):
     """
-     Make the conversion to the final formats, considering that the original
-     file is already syntactically checked
+    Make the conversion to the final formats, considering that the original
+    file is already syntactically checked
+
+    :param origin_file: origin file, where contains the data
+    :return: a dictionary, where the key is the ECF block key
     """
 
     # Open the source file
@@ -178,14 +230,58 @@ def make_conversion(origin_file, target_folder):
     return blocks
 
 
+def read_excel_header_templates(blocks, template_path):
+    """
+    Read the Excel file header and returns a dictionary indexed by the block key, in order to use the same column
+    names as used in the templates
+
+    :param blocks: a dictionary, where the key is the ECF block key
+    :param template_path: the path where there is the templates to read some information
+    :return:
+    """
+
+    # Initialize a dictionary, where the Key is the ECF Block Code
+    template_headers = {}
+
+    # Iterate over all the block keys, and export the list with all lines for
+    # a specific file
+    for block_key in blocks:
+        # Generate the target file name
+        file_name = template_path + block_key + TEMPLATE_EXCEL_EXTENSION
+
+        try:
+            # Read the excel file template
+            excel_df = pd.read_excel(file_name, sheet_name=TEMPLATE_EXCEL_SHEET_NAME)
+
+            # Get the column names from the template, and put them in a list
+            header_data = excel_df.columns.tolist()
+
+            # Put the list in the specific ECF key block
+            template_headers[block_key] = header_data
+        except FileNotFoundError:
+            print("File {0} not found. Skipping it! Please check!".format(file_name))
+
+    return template_headers
+
+
 def verify_source_exists(origin_file):
-    """ Verify that source file exists """
+    """
+    Verify if the source file exists
+
+    :param origin_file: origin file, where contains the data
+    :return: True - if the file exists ; False - if the file does not exist
+    """
 
     return os.path.exists(origin_file)
 
 
 def verify_target_exists(target_folder):
-    """ Verify that both target exist """
+    """
+    Verify if the target folder exist
+
+    :param target_folder: there folder to put the target files
+    :return: True - if the folder exists ; False - if the folder does not exist
+    """
 
     return os.path.exists(target_folder)
 
@@ -201,5 +297,6 @@ def verify_target_exists(target_folder):
 
 origin_file_path = "/home/rkruger/github.com/rodkruger/taxdatascience/tpdeloitte2guepardo/data/tpdeloitte.txt"
 target_path = "/home/rkruger/github.com/rodkruger/taxdatascience/tpdeloitte2guepardo/data/target/"
+template_path = "/home/rkruger/github.com/rodkruger/taxdatascience/tpdeloitte2guepardo/template/"
 
-convert_file(origin_file_path, target_path)
+convert_file(origin_file_path, target_path, template_path)
